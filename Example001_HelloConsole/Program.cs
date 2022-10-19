06e0202891e764d089ef75baa93f5603deb41a07
Elena <aksenovaea@mail.ru>
2022-10-19 16:28:37 +0300

    Первая задача

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,20 @@
 ﻿Console.WriteLine("2 Hello, World!");
+
+
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Программа вычесления квадрата числа");
+Console.WriteLine("Введите число");
+// 1. Ввод числа
+string inputString = Console.ReadLine()!;
+int input = int.Parse(inputString);
+
+// 2. Вычесление квадрата числа
+int result = input*input;
+
+// 3. Вывод результата на консоль
+Console.Write
---
+++ Example002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7;
+// a = 2; b = 10 -> max = 10;
+// a = -9; b = -3 -> max = -3;
+
+Console.WriteLine("программа вычисления min и max числа");
+Console.WriteLine("Введите 2 числа");
+
+int input1 = int.Parse(Console.ReadLine()!);
+int input2 = int.Parse(Console.ReadLine()!);
+   if (input1>input2)
+   {
+       Console.WriteLine("максимальное число-" + input1); 
+       Console.WriteLine("минимальное число-" + input2);
+   }
+    if (input1<input2)
+    {
+       Console.WriteLine("максимальное число-" + input2);
+       Console.WriteLine("минимальное число-" + input1);
+    }
