d08759b6883ec6610fd5da82b7a55bdc2e3a1a9d
Elena <aksenovaea@mail.ru>
2022-10-20 14:42:34 +0300

    Задача 6

---
+++ Example004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("программа вычисления min и max числа");
+Console.WriteLine("Введите 3 числа");
+
+int input1 = int.Parse(Console.ReadLine()!);
+int input2 = int.Parse(Console.ReadLine()!);
+int input3 = int.Parse(Console.ReadLine()!);
+int max = input1;
+   if (input2 > max) max=input2;
+   if (input3 > max) max=input3;
+
+       Console.WriteLine("максимальное число-" + max); 
+ 
+
+    
+  
+    
---
+++ Example006/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace homework
+{
+ 
+ // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+ 
+ class Program
+ {
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int a;
+        a = int.Parse(Console.ReadLine());
+        if (a % 2 == 0)
+        {
+            Console.WriteLine("число чётное");
+        }
+        else
+        {
+            Console.WriteLine("число нечётное");
+        }
+    }
+ }   
+}
